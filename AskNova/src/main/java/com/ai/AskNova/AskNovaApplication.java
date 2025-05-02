afd50048cc435f6e6141f15bce2e18c3d32a726d
harsh <harshpandey1957@gmail.com>
2025-05-02 11:55:25 +0530

    AskNova project is starting

---
+++ AskNova/src/main/java/com/ai/AskNova/AskNovaApplication.java
@@ -0,0 +1,13 @@
+package com.ai.AskNova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskNovaApplication.class, args);
+	}
+
+}
