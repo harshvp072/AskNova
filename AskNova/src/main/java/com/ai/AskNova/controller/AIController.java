6089aa64ad5f48303e8e105e04c85c1495130261
harsh <harshpandey1957@gmail.com>
2025-05-02 14:48:36 +0530

    backend of AskNova is ready

--- AskNova/src/main/java/com/ai/AskNova/controller/AIController.java
+++ AskNova/src/main/java/com/ai/AskNova/controller/AIController.java
@@ -3,6 +3,7 @@ package com.ai.AskNova.controller;
 import com.ai.AskNova.service.QnAService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/ask")
+@RequestMapping("/api/qna")
 @AllArgsConstructor
 public class AIController {
 
     private final QnAService qnAService;
 
+    @PostMapping("/ask")
     public ResponseEntity<String> askQuestions(@RequestBody Map<String,String> payLoad){
         String question = payLoad.get("question");
         String answer = qnAService.getAnswer(question);
--- AskNova/src/main/java/com/ai/AskNova/service/QnAService.java
+++ AskNova/src/main/java/com/ai/AskNova/service/QnAService.java
@@ -1,8 +1,14 @@
 package com.ai.AskNova.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Map;
+import java.util.Objects;
+
+//@AllArgsConstructor
 @Service
 public class QnAService {
 
@@ -12,13 +18,34 @@ public class QnAService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
+    private final WebClient webClient;
+
+    public QnAService(WebClient.Builder webClient){
+        this.webClient = webClient.build();
+    }
 
     public String getAnswer(String question) {
         // Construct the request payload
+        //{"contents": [{"parts":[{"text": "write code for adding two elements in java"}]}]}
+        Map<String, Object> requestBody = Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",question)
+                        })
+                }
+        );
+
 
         // Make API call
+        String response = webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
         // Return response
-        return  "";
+        return  response;
     }
 }
